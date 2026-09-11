e9c2a694dca5e9b439d4ecf5fbb581a6bfd4484e
Abiii <abiii@Abiiis-MacBook-Air.local>
2026-09-11 16:39:34 +0530

    Release 1.0.7

--- Package.swift
+++ Package.swift
@@ -16,13 +16,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SQLCipher",
-            url: "https://github.com/abishekan143-glitch/DevEnvironment/releases/download/1.0.3/SQLCipher.xcframework.zip",
+            url: "https://github.com/abishekan143-glitch/DevEnvironment/releases/download/1.0.7/Dev.xcframework.zip",
             checksum: "adfb3c7d129da4bd45f925ff8bd27f59dbe343e717b9e0e62c86f78c197b10a6"
         ),
         .binaryTarget(
             name: "Dev",
-            url: "https://github.com/abishekan143-glitch/DevEnvironment/releases/download/1.0.4/Dev.xcframework.zip",
-            checksum: "6783d890e231d33675ab33f19420bc809c0b057a7bc74d6f3c5d228b615f4b45"
+            url: "https://github.com/abishekan143-glitch/DevEnvironment/releases/download/1.0.6/Dev.xcframework.zip",
+            checksum: "747ad916b7a7c3ac0c7bc8d52fa701f894c37f517278d23f1a62d79d12ed6d7c"
         )
     ]
 )
