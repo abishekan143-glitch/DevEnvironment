909f843c5682eaa07624ef34693f77cd92098dff
Abiii <abiii@Abiiis-MacBook-Air.local>
2026-09-14 18:29:21 +0530

    Update Dev binary checksum

--- Package.swift
+++ Package.swift
@@ -22,11 +22,10 @@ let package = Package(
             url: "https://github.com/abishekan143-glitch/DevEnvironment/releases/download/1.0.3/SQLCipher.xcframework.zip",
             checksum: "adfb3c7d129da4bd45f925ff8bd27f59dbe343e717b9e0e62c86f78c197b10a6"
         ),
-
         .binaryTarget(
             name: "Dev",
             url: "https://github.com/abishekan143-glitch/DevEnvironment/releases/download/1.0.9/Dev.xcframework.zip",
-            checksum: "876b1f0956d65a8fbc2ac65f7c693d9e81f8083e886acf7a191c28c599438a75"
+            checksum: "aa00ebec14d6797f81b29a4ffe912cbf68e2856eaf0b79def051420dc86df866"
         )
     ]
 )
