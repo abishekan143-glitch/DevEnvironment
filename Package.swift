cf2aad0ba1757356b86e476f435e91b580b4f0d0
Abiii <abiii@Abiiis-MacBook-Air.local>
2026-09-14 18:47:26 +0530

    Update Dev binary to 1.1.1

--- Package.swift
+++ Package.swift
@@ -22,11 +22,10 @@ let package = Package(
             url: "https://github.com/abishekan143-glitch/DevEnvironment/releases/download/1.0.3/SQLCipher.xcframework.zip",
             checksum: "adfb3c7d129da4bd45f925ff8bd27f59dbe343e717b9e0e62c86f78c197b10a6"
         ),
-
-            .binaryTarget(
-                name: "Dev",
-                url: "https://github.com/abishekan143-glitch/DevEnvironment/releases/download/1.1.1/Dev.xcframework.zip",
-                checksum: "aa00ebec14d6797f81b29a4ffe912cbf68e2856eaf0b79def051420dc86df866"
-            )
+        .binaryTarget(
+            name: "Dev",
+            url: "https://github.com/abishekan143-glitch/DevEnvironment/releases/download/1.1.1/Dev.xcframework.zip",
+            checksum: "07efb62a2290600c73e877f26a218ef4c445737d2b8c90952ab8e626c32acf25"
+        )
     ]
 )
