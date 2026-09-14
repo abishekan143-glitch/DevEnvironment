47cc543b99a8be2a15a10636240be8346526638b
Abiii <abiii@Abiiis-MacBook-Air.local>
2026-09-14 18:40:52 +0530

    Update Dev binary to 1.1.1

--- Package.swift
+++ Package.swift
@@ -23,10 +23,10 @@ let package = Package(
             checksum: "adfb3c7d129da4bd45f925ff8bd27f59dbe343e717b9e0e62c86f78c197b10a6"
         ),
 
-        .binaryTarget(
-            name: "Dev",
-            url: "https://github.com/abishekan143-glitch/DevEnvironment/releases/download/1.1.0/Dev.xcframework.zip",
-            checksum: "aa00ebec14d6797f81b29a4ffe912cbf68e2856eaf0b79def051420dc86df866"
-        )
+            .binaryTarget(
+                name: "Dev",
+                url: "https://github.com/abishekan143-glitch/DevEnvironment/releases/download/1.1.1/Dev.xcframework.zip",
+                checksum: "aa00ebec14d6797f81b29a4ffe912cbf68e2856eaf0b79def051420dc86df866"
+            )
     ]
 )
